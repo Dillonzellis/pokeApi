43f8b76a015ea88fb841960a3a4340ea77424fde
Dillon Ellis <dillonzellis@gmail.com>
2023-05-20 19:55:08 -0400

    getting started

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <main className='bg-slate-900 text-white h-screen flex justify-center items-center'>
+      <div>yeppers</div>
+    </main>
+  );
+}
+
+export default App;
